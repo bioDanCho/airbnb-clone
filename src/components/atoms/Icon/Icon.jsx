abe549b202e1d47bcb5bf71ac1c36dd11eb1fd97
bioDanCho <daniel.cho@thebioagency.com>
2022-09-05 11:51:21 -0400

    search bar

---
+++ src/components/atoms/Icon/Icon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Icon = (props) => {
+    const { children: iconEl } = props;
+
+    return <div>{iconEl}</div>;
+};
+
+export default Icon;
--- src/components/molecules/ClickableSearchBar/ClickableSearchBar.jsx
+++ src/components/molecules/ClickableSearchBar/ClickableSearchBar.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import './ClickableSearchBar.scss';
-import { FaBeer } from 'react-icons/fa';
+import { BiSearch } from 'react-icons/bi';
+import Icon from '../../atoms/Icon/Icon';
 
-const ClickableSearchbar = () => {
+const ClickableSearchBar = () => {
     return (
         <div className='container'>
-            <FaBeer />
-            <div className='text'>
-                <div className='where'>Where to?</div>
-                <div className='any'>Anywhere • Any week • Add guests</div>
+            <div className='search'>
+                <div className='icon'>
+                    <Icon>
+                        <BiSearch />
+                    </Icon>
+                </div>
+                <div className='text'>
+                    <div className='where'>Where to?</div>
+                    <div className='any'>Anywhere • Any week • Add guests</div>
+                </div>
             </div>
-            <FaBeer />
+            <div className='settings'>
+                <BiSearch />
+            </div>
         </div>
     );
 };
 
-export default ClickableSearchbar;
+export default ClickableSearchBar;
